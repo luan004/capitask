5fb46d0d71c73776b847bbe13afc5753a15d8e5a
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-10-31 16:12:05 -0300

    31-10

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <>
+            <h2>Teste</h2>
+            <button>Teste</button>
+        </>
+    );
+}
+
+export default App;
